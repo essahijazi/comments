e2b221823b54130d5a0e75d85a8483ed9234de59
Hijazi <ctcrdneh@opm.gov>
2020-02-10 22:41:39 -0500

    added different types of comments

---
+++ Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+	/*
+	 * This is the first line of the comment
+	 * This is the second line of the comment
+	 * This is the third line of the comment
+	 * */
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World One");// Line comment
+		
+		
+		System.out.println("Hello World Two");
+		
+		/**
+		 * 
+		 * This is a JavaDoc comment
+		 * JavaDoc utility allows you to generate documentation
+		 * using this special comment format
+		 * 
+		 * */
+		System.out.println("Hello World Three");
+		
+	}
+
+}
